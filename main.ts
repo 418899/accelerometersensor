7134323017539d39321be5fb915c5662e3d3ced3
418899 <115142712+418899@users.noreply.github.com>
2023-01-05 16:46:43 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,24 @@
+let accy = 0
+let accx = 0
+let x = 2
+let y = 2
+led.plot(x, y)
+basic.forever(function () {
+    accx = input.acceleration(Dimension.X)
+    accy = input.acceleration(Dimension.Y)
+    if (accx > 200 && x > 0) {
+        x = x - 1
+    }
+    if (accx < -200 && x < 4) {
+        x = x + 1
+    }
+    if (accy > 200 && y > 0) {
+        y = y - 1
+    }
+    if (accy < -200 && y < 4) {
+        y = y + 1
+    }
+    basic.clearScreen()
+    led.plot(x, y)
+    basic.pause(100)
+})
